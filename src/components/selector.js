f5c9ff6bef0455e0a91d8fb20ddbfc3fd315a807
Adrien Martin <adrien.v.martin@gmail.com>
2022-10-09 12:10:33 -0600

    Create selector component

---
+++ src/components/selector.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Paper} from '@mui/material';
+
+const Selector = () => {
+  return (
+    <Paper elevation={2}>
+      Selector Component
+    </Paper>
+  )
+}
+
+export default Selector;
