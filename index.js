ca1ba1b8d39c32611ef6ac7032d6a3906f0de20c
Adrien Martin <adrien.v.martin@gmail.com>
2022-10-17 19:21:36 -0600

    Remove default coding

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
+// Create endpoint here and use fs to read and parse CSV data, and then send that data to the frontend through the endpoint
+app.post('/data', (req, res) => {
+  res.send('Create parser function with fs here');
+});
+
 app.get('/', (req, res) => {
   res.send('Connected! Yay!')
-})  
+});
