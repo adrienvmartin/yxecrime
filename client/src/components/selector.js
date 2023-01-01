0eb5318df55f39db894c4e32a2b30d493f2f54e1
Adrien Martin <adrien.v.martin@gmail.com>
2023-01-01 16:31:51 -0600

    Remove unused imports

--- client/src/components/selector.js
+++ client/src/components/selector.js
@@ -1,14 +1,11 @@
 import React, { Fragment } from 'react';
-import { Paper } from '@mui/material';
-import Autocomplete from '@mui/material/Autocomplete';
-import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import getNeighbourhoods from '../data/neighbourhoods';
 
 const Selector = () => {
   return (
     <Fragment>
-      
+
       <br />
       <Button onClick={() => { getNeighbourhoods(); }}>Get Neighbourhoods</Button>
       </Fragment>
