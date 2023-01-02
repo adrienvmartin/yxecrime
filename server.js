e0bd45810b105cc0761170f95c350f8efb2a5eda
Adrien Martin <adrien.v.martin@gmail.com>
2023-01-01 19:09:33 -0600

    Rough version - passing csv data to frontend

--- server.js
+++ server.js
@@ -14,22 +14,42 @@ app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
-// Create endpoint here and use fs to read and parse CSV data, and then send that data to the frontend through the endpoint
-app.get('/data', (req, res) => {
+const newlist = [];
+
+const processData = (data) => {
+
+  const csvData = [];
+  csvData.push(data);
+
+  csvData[0].forEach(e => {
+    newlist.push(e);
+  })
+
+  // console.log('processData results: ', csvData[0].length);
+  // console.log('newlist: ', newlist);
+  return csvData;
+}
+
+const readCSV = async () => {
   const csvFilePath = 'client/src/data/SaskNeighbourhoods.csv';
   const file = fs.createReadStream(csvFilePath);
-  const csvData = [];
 
-  papa.parse(file, {
+  await papa.parse(file, {
     header: true,
-    step: (res) => {
-      csvData.push(res.data);
-    },
-    complete: (res, file) => {
-      console.log('Complete', csvData.length, 'records');
-      console.log('Records: ', csvData[0]);
+     /* step: (res) => {
+    }, */
+    complete: (res) => {
+      processData(res.data);
     }
-  })
+  });
+  return newlist;
+}
+
+// Create endpoint here and use fs to read and parse CSV data, and then send that data to the frontend through the endpoint
+app.get('/data', (req, res) => {
+  readCSV();
+  // console.log('newlist at api[0] ', newlist[1])
+  res.send(newlist);
 });
 
 app.get('/', (req, res) => {
