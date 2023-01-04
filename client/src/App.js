ba8bda20e67b1e2f787af3c803c972d332519c2a
Adrien Martin <adrien.v.martin@gmail.com>
2023-01-03 19:45:13 -0600

    Move data fetching to main App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Grid2 from '@mui/material/Unstable_Grid2';
 import Selector from './components/selector';
+import axios from "axios";
 
+const getData = axios.get('http://localhost:3001/data').then(r => {
+  return r.data
+});
 function App() {
+  const [data, setData] = useState({});
+
+  const loadStats = () => {
+    getData.then((d) => {
+      setData(d);
+      console.log('d = ', d)
+    })
+  }
+
+  useEffect(() => {
+   loadStats();
+    // console.log('newdata', data);
+  }, [])
   return (
     <Fragment>
       <Grid2 xs={12} container direction="column" justify="center">
