5ddb7b87c2824f6510b8c459aeaf95338914dd0f
Adrien Martin <adrien.v.martin@gmail.com>
2023-01-03 19:17:02 -0600

    Retrieve data from response

--- client/src/data/neighbourhoods.js
+++ client/src/data/neighbourhoods.js
@@ -3,8 +3,11 @@ import papa from "papaparse";
 
 const  getNeighbourhoods = () => {
 
-  axios.get('http://localhost:3001/data').then(r => console.log(r.data));
+  const data = axios.get('http://localhost:3001/data').then(r => {
+    return r.data
+  });
 
+  console.log('data: ', data);
 }
 
 export default getNeighbourhoods;
